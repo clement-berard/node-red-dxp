fab3193ca7caccbac4844c64aec369cdde3be13a
Clément BERARD <contact@clementberard.com>
2024-11-19 14:55:54 +0100

    up

--- src/builder/index.ts
+++ src/builder/index.ts
@@ -1,4 +1,3 @@
-import * as fsPromise from 'node:fs/promises';
 import { getCurrentInstance } from '../current-instance';
 import { buildNodeController } from './esbuild';
 import { buildFinalDistIndexContent } from './final-index';
@@ -12,18 +11,6 @@ export * from './styles';
 export * from './html';
 export * from './final-index';
 export * from './doc';
-
-export async function writeCacheConfigFile(): Promise<void> {
-  try {
-    await fsPromise.writeFile(
-      `${currentInstance.pathLibCacheDir}/config.json`,
-      JSON.stringify(currentInstance.config, null, 2),
-      'utf-8',
-    );
-  } catch (error) {
-    console.error('Error writing cache config file:', error);
-  }
-}
 
 type BuildAllPackageParams = {
   minify?: boolean;
@@ -49,20 +36,6 @@ export async function buildAllPackage(params?: BuildAllPackageParams): Promise<v
         minify,
         nodes: nodeFoldersDefinition,
       }).then(async (contentFinalIndexHtml) => {
-        // const extractor = new CSSExtractor({
-        //   ...options,
-        //   sources: [`${currentInstance.pathDist}`],
-        //   include: ['**/*.{html}'],
-        // });
-        //
-        // extractor.init();
-        // await extractor.prepare();
-        // const resolvedVirtualModuleId = extractor.;
-        // console.log('extractor', resolvedVirtualModuleId);
-        // await extractCSSFromHTML(contentFinalIndexHtml, `${currentInstance.pathDist}/index.css}`, {
-        //   verbose: 1,
-        // });
-
         await writeFile(`${currentInstance.pathDist}/index.html`, contentFinalIndexHtml);
       });
     });
