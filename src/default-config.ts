3c0735abe52dec9e9879180db487ef1585a46744
Clément BERARD <contact@clementberard.com>
2024-11-30 08:56:04 +0100

    refactor: clean some code and improve styles part (#51)

--- src/default-config.ts
+++ src/default-config.ts
@@ -4,6 +4,9 @@ export type Config = {
     esbuildControllerOptions?: {
       includeInBundle?: string[];
     };
+    tailwind?: {
+      forcedClassesInclusion?: string[];
+    };
   };
   watcher: {
     nodeRed: {
@@ -19,6 +22,9 @@ export const defaultConfig: Partial<Config> = {
     outputDir: 'dist',
     esbuildControllerOptions: {
       includeInBundle: [],
+    },
+    tailwind: {
+      forcedClassesInclusion: [],
     },
   },
   watcher: {
