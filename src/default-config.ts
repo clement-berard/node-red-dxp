15b9c5b1a8c06f4a5a7de1494654f4360770dcaa
Clément BERARD <contact@clementberard.com>
2026-01-03 21:52:41 +0100

    up

--- src/default-config.ts
+++ src/default-config.ts
@@ -1,50 +1,56 @@
-export type Config = {
-  builder?: {
-    outputDir: string;
-    esbuildControllerOptions?: {
-      includeInBundle?: string[];
-    };
-    tailwind?: {
-      forcedClassesInclusion?: string[];
-    };
-    editor?: {
-      webComponents?: {
-        /**
-         * @default false
-         */
-        formRow?: boolean;
-      };
-    };
-  };
-  watcher: {
-    nodeRed: {
-      enabled: boolean;
-      path: string | undefined;
-      url: string;
-    };
-  };
-};
+import { z } from 'zod';
 
-export const defaultConfig: Partial<Config> = {
-  builder: {
-    outputDir: 'dist',
-    esbuildControllerOptions: {
-      includeInBundle: [],
-    },
-    tailwind: {
-      forcedClassesInclusion: [],
-    },
-    editor: {
-      webComponents: {
-        formRow: false,
-      },
-    },
-  },
-  watcher: {
-    nodeRed: {
-      enabled: true,
-      path: '~/.node-red',
-      url: 'http://localhost:1880',
-    },
-  },
-};
+export const ConfigSchema = z
+  .object({
+    builder: z
+      .object({
+        outputDir: z.string().default('dist'),
+        esbuildControllerOptions: z
+          .object({
+            includeInBundle: z.array(z.string()).default([]),
+          })
+          .strict()
+          .default({
+            includeInBundle: [],
+          }),
+        tailwind: z
+          .object({
+            forcedClassesInclusion: z.array(z.string()).default([]),
+          })
+          .strict()
+          .default({
+            forcedClassesInclusion: [],
+          }),
+      })
+      .strict()
+      .default({
+        outputDir: 'dist',
+        esbuildControllerOptions: {
+          includeInBundle: [],
+        },
+        tailwind: {
+          forcedClassesInclusion: [],
+        },
+      }),
+    watcher: z
+      .object({
+        nodeRed: z
+          .object({
+            enabled: z.boolean().default(true),
+            path: z.string().default('~/.node-red'),
+            url: z.url().default('http://localhost:1880'),
+          })
+          .strict(),
+      })
+      .strict()
+      .default({
+        nodeRed: {
+          enabled: true,
+          path: '~/.node-red',
+          url: 'http://localhost:1880',
+        },
+      }),
+  })
+  .strict();
+
+export const defaultConfig = ConfigSchema.parse({});
