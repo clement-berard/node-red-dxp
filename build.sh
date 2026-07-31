23a0df1e7a9787ac316997d8a329deb0417aea89
Clément BERARD <contact@clementberard.com>
2026-07-31 07:36:45 +0200

    up

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ SOURCE="./src/cli/commands/create-node"
 DESTINATION="./dist/scaffolding/create-node"
 
 mkdir -p "$DESTINATION"
-rsync -a --include='*.hbs' --include='*/' --exclude='*' "$SOURCE/" "$DESTINATION/"
+rsync -a --exclude='__tests__/' --include='*.hbs' --include='*/' --exclude='*' "$SOURCE/" "$DESTINATION/"
 
 cp -r ./src/editor/assets dist/editor
 
